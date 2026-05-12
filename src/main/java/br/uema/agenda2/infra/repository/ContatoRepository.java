b222b921ff8c8c4da5a67a5f2512ef4ae6a1a9dc
angelolfreitas <angeloantonio202020@gmail.com>
2026-05-12 17:34:09 -0300

    segundo

--- src/main/java/br/uema/agenda2/infra/repository/ContatoRepository.java
+++ src/main/java/br/uema/agenda2/infra/repository/ContatoRepository.java
@@ -1,4 +1,7 @@
 package br.uema.agenda2.infra.repository;
 
-public interface ContatoRepository {
+import br.uema.agenda2.infra.entity.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
 }
---
+++ src/main/java/br/uema/agenda2/service/ContatoService.java
@@ -0,0 +1,8 @@
+package br.uema.agenda2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContatoService {
+    private
+}
